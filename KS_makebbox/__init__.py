62ac27a885efb538b78868cfd2d6e6ad110b8bb8
KeesungLee <ks.lee@alcherainc.com>
2022-11-14 13:41:29 +0900

    v0.0.2

--- KS_makebbox/__init__.py
+++ KS_makebbox/__init__.py
@@ -48,3 +48,36 @@ def make_point(img: np.ndarray, x: int, y: int, color: tuple=(0, 0, 255), outlin
         img = cv2.line(img, (x, y), (x,  y), color=(0, 0, 0), thickness = thickness + 2)
     img = cv2.line(img, (x, y), (x,  y), color=color, thickness = thickness)
     return img
+
+def make_polygon(img: np.ndarray, polygon: list, color: tuple=(0,0,255)):
+    """_summary_
+
+    Args:
+        img (np.ndarray): cv2 이미지(numpy)
+        polygon (list): 폴리곤 값이 담긴 리스트
+        color (tuple, optional): 색상. Defaults to (0, 0, 255).
+
+    Returns:
+        np.ndarray: cv2 이미지
+    """    
+    polygon = np.array(polygon).reshape(int(len(polygon)/2),2)
+    img = cv2.fillPoly(img, [polygon], color)
+    return img
+
+def img_combine_polygon(ori_img: np.ndarray, combine_img: np.ndarray, polygon_list: list):
+    """
+    ori_img에 combine_img의 polygon 값을 복사한다.
+
+    Args:
+        ori_img (np.array): 원본 이미지
+        combine_img (np.array): 병합 이미지
+        polygon_list (list): np.ndarray로 이루어진 폴리곤 list
+
+    Returns:
+        _type_: np.array
+    """    
+    mask = np.zeros(ori_img.shape)
+    mask = cv2.fillPoly(mask, polygon_list, (255, 255, 255))
+    index_arr = np.where(mask == 255)
+    ori_img[index_arr] = combine_img[index_arr]
+    return ori_img
